f38a2e043514c5e2b977688dee4d2623a612cc60
Niklles <max777944@inbox.ru>
2024-03-01 20:45:09 +0300

    third commit

--- src/Tasks/task3/model/Matrix.java
+++ src/Tasks/task3/model/Matrix.java
@@ -2,17 +2,52 @@ package src.tasks.task3.model;
 
 public class Matrix {
 
-    double[][] table = new double [][] 
-    {
-        {1.5, 2.5, 3.5},
-        {4.5, 5.5, 6.5}
-    };
+    double[][] table = new double[2][3];
+    public Matrix(double[][] table){
+        this.table = table;
+        
+    }
+    
 
-    for (int i = 0; i < table.length; i++){
-        for(int j = 0; j < table[i].length; j++){
-                 
-            System.out.printf("%.2f ", table[i][j]);
+
+    public Matrix sum(Matrix other){
+        double[][] result = new double[2][3];
+        for (int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                result[i][j] = this.table[i][j] + other.table [i][j];
+            }
         }
-        System.out.println();
+        Matrix sumAll = new Matrix(result);
+        return sumAll;
+        
+    }
+
+    public Matrix multiply(int num){
+        for (int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                this.table[i][j] = this.table[i][j] * num;
+            }
+        }
+        Matrix multiplyall = new Matrix(table);
+        return multiplyall;
+    }
+
+    public Matrix multiplicationOfTables(Matrix another){
+        double[][] answer = new double[2][3];
+        for (int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                answer[i][j] = this.table[i][j] * another.table [i][j];
+            }
+        }
+        Matrix mulOfTab = new Matrix(answer);
+        return mulOfTab;
+    }
+
+    public void displayInfo(){ 
+        for (int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                System.out.printf("%.2f \n", table [i][j]);
+            }
+        }
     }
 }
--- src/Tasks/task3/task3.java
+++ src/Tasks/task3/task3.java
@@ -4,6 +4,19 @@ import src.tasks.task3.model.*;
 
 public class task3 {
     public static void Execute() {
-        double[][] table = {{1.5, 2.5, 3.5}, {4.5, 5.5, 6.5}};
+        Matrix table1 = new Matrix(new double[][] {{1.5, 2.5, 3.5}, {4.5, 5.5, 6.5}});
+
+        Matrix table2 = new Matrix(new double[][] {{1.8, 2.8, 3.8}, {4.8, 5.8, 6.8}});
+        Matrix all;
+
+        all = table1.sum(table2);
+        all.displayInfo();
+        table1.displayInfo();
+        table2.displayInfo();
+        table1.multiply(10);
+        table2.multiply(5);
+        table1.multiplicationOfTables(table2);
+        
+
     }
 }
