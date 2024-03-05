14ef061ac068cc222444eacece4bade2e87a714d
Niklles <max777944@inbox.ru>
2024-03-05 03:54:49 +0300

    six commit

--- Programm.java
+++ Programm.java
@@ -3,6 +3,7 @@ import src.tasks.task2.task2;
 import src.tasks.task3.task3;
 import src.tasks.task4.task4;
 import src.tasks.task5.task5;
+import src.tasks.task6.task6;
 
 public class Programm {
 
@@ -11,8 +12,8 @@ public class Programm {
         // task2.Execute();
         // task3.Execute();
         // task4.Execute();
-        task5.Execute();
-        // task6.Execute();
+        // task5.Execute();
+        task6.Execute();
         // task7.Execute();
         // task8.Execute();
         // task9.Execute();
