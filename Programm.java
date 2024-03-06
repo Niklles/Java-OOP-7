cbf0870a0517467c55a37aaed2f225c27ed8a683
Niklles <max777944@inbox.ru>
2024-03-06 18:15:32 +0300

    seventh commit

--- Programm.java
+++ Programm.java
@@ -4,6 +4,7 @@ import src.tasks.task3.task3;
 import src.tasks.task4.task4;
 import src.tasks.task5.task5;
 import src.tasks.task6.task6;
+import src.tasks.task7.task7;
 
 public class Programm {
 
@@ -13,8 +14,8 @@ public class Programm {
         // task3.Execute();
         // task4.Execute();
         // task5.Execute();
-        task6.Execute();
-        // task7.Execute();
+        // task6.Execute();
+        task7.Execute();
         // task8.Execute();
         // task9.Execute();
         // task10.Execute();
